6122ad4a3efaccd9ee8b0abe7d0edc2396dab6e4
jiahao <jiahao@mogu.com>
2020-06-09 21:13:30 +0800

    两个原地移除数组元素的问题

---
+++ alg/src/main/java/leetcode/easy/RemoveElementArray.java
@@ -0,0 +1,90 @@
+package leetcode.easy;
+
+/**
+ *给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ *
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 给定 nums = [3,2,2,3], val = 3,
+ *
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ *
+ * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ *
+ * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ *
+ * 注意这五个元素可为任意顺序。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author jiahao
+ * @Date 2020/6/9 20:33
+ */
+public class RemoveElementArray {
+
+    /**
+     * 优雅的解法
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement1(int[] nums, int val){
+        int i = 0;
+        for (int j = 0; j < nums.length;j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    // 自己做的，有点不优雅
+    public static int removeElement(int[] nums, int val) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int temp;
+        int j = nums.length - 1;
+        int i = 0;
+        while (i <= j){
+            if(nums[i] == val){// 遇到相同的，就和最后一个元素替换
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j--;
+            }else {
+                i++;
+            }
+        }
+
+        if(j < 0 || nums[j] == val){// 排除所有元素都重复的情况
+            return 0;
+        }
+        return j + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3};
+        int i = removeElement(nums,3);
+
+        int in = 0;
+        while (in < i){
+            System.out.println(nums[in++]);
+        }
+    }
+}
--- alg/src/main/java/linkedList/DoTest.java
+++ alg/src/main/java/linkedList/DoTest.java
@@ -1,5 +1,8 @@
 package linkedList;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * TODO
  *
@@ -8,14 +11,6 @@ package linkedList;
  */
 public class DoTest {
     public static void main(String[] args) {
-        int targetPrice = 400;// 400分 = 4元
-        int roi = 400;// roi 数据库里扩大100倍的
-        int price = 1;// 0.01 元
-        int deltaPrice = targetPrice - price;
-        int cashBack = deltaPrice * 100 / roi;// 转化为分
-        System.out.println("cashBack:" + cashBack);
 
-        long amount = Math.round(cashBack / 100.0) > 0 ? Math.round(cashBack / 100.0) : 1;
-        System.out.println("amount:" + amount);
     }
 }
