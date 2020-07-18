17b7a3dcc4ae6729e118b47515917733099b4d01
jiahao <jiahao@mogu.com>
2020-07-18 10:11:21 +0800

    跳台阶问题

---
+++ alg/src/main/java/leetcode/easy/ClimbStairs.java
@@ -0,0 +1,50 @@
+package leetcode.easy;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author jiahao
+ * @Date 2020/7/18 9:54
+ */
+public class ClimbStairs {
+    public static int climbStairs(int n) {
+        int n1 = 1;
+        int n2 = 1;
+        int temp;
+        // 每一项是前两项的和
+        for(int i = 2; i <= n;i++ ){
+            temp = n2;
+            n2 = n1 + n2;
+            n1 = temp;
+        }
+        return n2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+}
