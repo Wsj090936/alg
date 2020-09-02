49b0a6ccd30d983cb7377b2fd703906dd5376f1e
jiahao <jiahao@mogu.com>
2020-09-02 20:09:12 +0800

    one easy

---
+++ alg/src/main/java/leetcode/base/TreeNode.java
@@ -0,0 +1,38 @@
+package leetcode.base;
+
+/**
+ *
+ * @Author jiahao
+ * @Date 2020/9/2 19:30
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int x) { val = x; }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
